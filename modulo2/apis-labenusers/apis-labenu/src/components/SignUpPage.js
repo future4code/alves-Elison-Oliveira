9797e119f21b976d34e2d15e54d0cf5519cb45df
Elison Moises da Silva Oliveira <ajatosdownload@gmail.com>
2022-06-22 21:38:59 -0300

    adicionando arquivo editado apis-labenusers

--- modulo2/apis-labenusers/apis-labenu/src/components/SignUpPage.js
+++ modulo2/apis-labenusers/apis-labenu/src/components/SignUpPage.js
@@ -1,5 +1,41 @@
 import React from "react";
 import axios from "axios";
+import styled from "styled-components";
+
+const Botao = styled.button`
+  margin: 0 auto;
+
+  border: none;
+  background-color: hsl(250, 35%, 26%);
+  z-index: 1;
+  position: relative;
+  font-size: inherit;
+  font-family: inherit;
+  color: black;
+  padding: 0.5em 1em;
+  outline: none;
+  border: none;
+  overflow: hidden;
+  cursor: pointer;
+`;
+
+const Login = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 25%;
+  margin: 10px;
+  border: 1px solid black;
+  padding: 20px;
+  margin: auto;
+  border-radius: 10px;
+  box-shadow: 1px 1px 5px;
+  border: none;
+  height: 33vh;
+  input {
+    margin: 5px;
+    height: 30px;
+  }
+`;
 
 class SignUpPage extends React.Component {
   state = {
@@ -20,7 +56,7 @@ class SignUpPage extends React.Component {
   };
 
   handleCreateUser = () => {
-    const axiosConfig = {
+    const newAxios = {
       headers: {
         Authorization: "Elison-Moises-Alves",
       },
@@ -35,7 +71,7 @@ class SignUpPage extends React.Component {
       .post(
         "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
         body,
-        axiosConfig
+        newAxios
       )
       .then(() => {
         alert(`Usuário ${this.state.name} criado com sucesso!`);
@@ -50,19 +86,22 @@ class SignUpPage extends React.Component {
   render() {
     return (
       <div>
-        <input
-          placeholder="Nome"
-          type="text"
-          value={this.state.name}
-          onChange={this.handleNameChange}
-        />
-        <input
-          placeholder="E-mail"
-          type="email"
-          value={this.state.email}
-          onChange={this.handleEmailChange}
-        />
-        <button onClick={this.handleCreateUser}>Criar Usuário</button>
+        <Login>
+          <h2>Cadastro</h2>
+          <input
+            placeholder="Nome"
+            type="text"
+            value={this.state.name}
+            onChange={this.handleNameChange}
+          />
+          <input
+            placeholder="E-mail"
+            type="email"
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+          />
+          <Botao onClick={this.handleCreateUser}>Cadastra</Botao>
+        </Login>
       </div>
     );
   }
