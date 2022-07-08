9ec8e49ae8542fa32c2eac998f7703ea03ad8dd7
Elison Moises da Silva Oliveira <ajatosdownload@gmail.com>
2022-07-08 19:15:41 -0300

    primeira parte do projeto astromatch

---
+++ modulo3/projeto-astromatch/astromatch/src/pages/MatchesPage.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { API_CLIENT, BASE_URL } from "../constants/urls";
+
+function MatchesPage() {
+  const [matches, setMatches] = useState(undefined);
+
+ 
+  useEffect(() => {
+    getMatches();
+  }, []);
+
+  const getMatches = () => {
+    const url = `${BASE_URL}/${API_CLIENT}/matches`;
+
+    axios
+      .get(url)
+      .then((res) => {
+        setMatches(res.data.matches);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  const allMatches =
+    matches &&
+    matches.map((match) => {
+      return (
+        <figure key={match.id}>
+          <img
+            src={match.photo}
+            alt={`foto de ${match.name}`}
+            height={"32px"}
+          ></img>
+          <span>{match.name}</span>
+          <hr />
+        </figure>
+      );
+    });
+
+  return (
+    <>
+      <h2>Matches</h2>
+      {allMatches}
+    </>
+  );
+}
+
+export default MatchesPage;
