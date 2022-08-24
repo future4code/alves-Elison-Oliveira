918c633ce5c369681279b452bcf91ab03a532c92
Elison Moises da Silva Oliveira <ajatosdownload@gmail.com>
2022-08-24 20:37:06 -0300

    adicionando introducao-sql

---
+++ modulo4/introducao-sql/labenu.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Actor (
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR (255) NOT NULL,
+    salary FLOAT NOT NULL,
+    birth_date DATE NOT NULL,
+    gender VARCHAR(6) NOT NULL
+);
+
+-- varchar forma de limita caracteres
+
+
+INSERT INTO Actor (id, name, salary, birth_date, gender)
+VALUES(
+  "001", 
+  "Tony Ramos",
+  400000,
+  "1948-08-25", 
+  "male"
+);
